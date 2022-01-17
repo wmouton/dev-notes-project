d6341953d2ad3c9ef5c2eb8c5ce6137fbc0e82da
l33th <l33th@protonmail.ch>
2022-01-17 07:23:40 +0200

    display notes data

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import { Note } from '../models/note.model';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 interface INotesProps {
   note: Note;
 }
 
-const Notes: React.FunctionComponent<INotesProps> = props => {
-  return <div>Note Card</div>;
+const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
+  return (
+    <div className='mb-3'>
+      <Card style={{backgroundColor: note.color}}>
+        <Card.Body>
+          <Card.Title>{note.title}</Card.Title>
+          <Card.Text>{note.text}</Card.Text>
+          <Card.Subtitle className='text-muted'>{note.date}</Card.Subtitle>
+          <Button className='mt-3' variant='danger'>
+            Delete
+          </Button>
+          {/* <Card.Link href='#'>Card Link</Card.Link>
+          <Card.Link href='#'>Another Link</Card.Link> */}
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Notes;
