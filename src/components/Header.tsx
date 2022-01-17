28c0e659b2e9b6e691d17e9aef50fa2a8a2e3d28
l33th <l33th@protonmail.ch>
2022-01-17 06:46:00 +0200

    display header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 
 interface IHeaderProps {}
 
 const Header: React.FunctionComponent<IHeaderProps> = props => {
   return(
-    <div></div>
+    <Navbar fixed='top' bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand>
+          Dev Notes - by WMouton
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 }
 
