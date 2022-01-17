81c1a2464c2ece51f922b77ecef269a37368b913
l33th <l33th@protonmail.ch>
2022-01-17 06:23:01 +0200

    state setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 // Routing
 // @ts-ignore
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Components
 import Header from './components/Header';
-import NotFound from './components/NotFound';
+// import NotFound from './components/NotFound';
 
-const App: React.FC = () => (
-  <Router>
-    <Routes>
-      <Route path='/' element={<Header />} />
-      <Route path='/*' element={<NotFound />} />
-    </Routes>
-  </Router>
-);
+const App: React.FC = () => {
+  const [notes, setNotes] = useState<{
+    id:string,
+    title:string,
+    text:string,
+    color:string,
+    date:string
+  }[]>([{
+    id: (new Date()).toString(),
+    title: 'Applications',
+    text: 'Schedule application launch.',
+    color: '#dfdfdf',
+    date: (new Date()).toString()
+  }]);
+
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
+};
 
 export default App;
