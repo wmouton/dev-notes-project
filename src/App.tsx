d9da4459773c065dc6c26c18711a109886f1d185
l33th <l33th@protonmail.ch>
2022-01-17 06:58:20 +0200

    display notes component data

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
   return (
     <>
       <Header />
-      <Container>
+      <Container className='mt-5'>
         <Row>
           <Col>
             <NotesList />
