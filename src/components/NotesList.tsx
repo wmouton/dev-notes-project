2dc13ab712262f49b2813cae927edee1c787a93e
l33th <l33th@protonmail.ch>
2022-01-17 07:42:50 +0200

    added delete functionality

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -3,13 +3,16 @@ import { Note } from '../models/note.model';
 import Notes from './Notes';
 
 interface INotesListProps {
-  notes: Note[],
-  setNotes: React.Dispatch<React.SetStateAction<Note[]>>
+  notes: Note[];
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
 }
 
-const NotesList: React.FunctionComponent<INotesListProps> = ({ notes }) => {
+const NotesList: React.FunctionComponent<INotesListProps> = ({
+  notes,
+  setNotes,
+}) => {
   const handleDelete = (id: string) => {
-    console.log('The note to be deleted', id);
+    setNotes(notes.filter(note => note.id !== id));
   };
   const renderNotes = (): JSX.Element[] => {
     return notes.map(note => {
